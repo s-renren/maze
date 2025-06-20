15165f5077cc650eb95aa9c38a467ddd54bbff51
s-renren <s1f102301156@iniad.org>
2025-06-21 02:45:19 +0900

    向きに合わせたユーザーの表示

--- src/components/board/page.tsx
+++ src/components/board/page.tsx
@@ -5,7 +5,7 @@ import styles from './page.module.css';
 
 export default function Board() {
   const initialBoard: number[][] = [
-    [2, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 1, 0, 1, 0, 1, 0, 1, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 1, 0, 1, 0, 1, 0, 1, 0],
@@ -15,6 +15,10 @@ export default function Board() {
     [0, 1, 0, 1, 0, 1, 0, 1, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
   ];
+
+  const [user, setUser] = useState({ x: 0, y: 0, direction: 1 });
+
+  const directionNames = ['Up', 'Right', 'Down', 'Left'];
   const directions = [
     { x: 0, y: -1 }, // 上
     { x: 1, y: 0 }, // 右
@@ -23,6 +27,7 @@ export default function Board() {
   ];
   const [board, setBoard] = useState(initialBoard);
 
+  // 迷路生成
   function knockdownObstacle() {
     const newBoard = structuredClone(initialBoard);
     const result = newBoard.flatMap((row, y) =>
@@ -37,19 +42,30 @@ export default function Board() {
         newBoard[newY][newX] = 1;
       }
     });
-
     setBoard(newBoard);
   }
 
   return (
     <div>
       <button onClick={knockdownObstacle}>迷路生成</button>
+      <button>プレイヤーを進める</button>
       <div className={styles.board}>
         {board.map((row, rowIndex) => (
-          <div key={rowIndex}>
-            {row.map((cell, cellIndex) => (
-              <div key={cellIndex} className={cell === 0 ? styles.road : styles.obstacle} />
-            ))}
+          <div key={rowIndex} className={styles.row}>
+            {row.map((cell, cellIndex) => {
+              const isUser = user.x === cellIndex && user.y === rowIndex;
+              const cellClass = cell === 1 ? styles.obstacle : styles.road;
+              const directionName = directionNames[user.direction];
+              return (
+                <div key={cellIndex} className={`${cellClass}`}>
+                  {isUser && (
+                    <div className={styles.user}>
+                      <div className={styles[`user${directionName}`]} />
+                    </div>
+                  )}
+                </div>
+              );
+            })}
           </div>
         ))}
       </div>
